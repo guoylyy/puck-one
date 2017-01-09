629bf9638a02a4e1616daa8f82e16b285580a9b1
oliverbi <bixiaodong1990@163.com>
2017-01-10 02:02:28 +0800

    文本聊天主要逻辑完成

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,11 +1,64 @@
 define(function (require, exports, module) {
 	var chatPage = {
-		init: function () {
-			this.bindEvents();
+		initialize: function () {
+			this.initConversation();
 			$.init();
 		},
+		initConversation: function () {
+			var self = this;
+
+			// 初始化存储 SDK
+			AV.init({
+			  	appId: 'XJroNb2m8eHOGobg6WKN16K6-gzGzoHsz', 
+			  	appKey:'rGGy6ziKb0uxnfrwp6T37gmK',
+			});
+			// 初始化实时通讯 SDK
+			var Realtime = AV.Realtime;
+			var realtime = new Realtime({
+			  	appId: 'XJroNb2m8eHOGobg6WKN16K6-gzGzoHsz',
+			  	region: 'cn',
+			  	plugins: [AV.TypedMessagesPlugin], // 注册富媒体消息插件
+			});
+
+			/*
+				需要替换为实际进行对话的用户的唯一标识
+			 */
+			var clientID1 = 'Oliver', clientID2 = 'Mikey';
+			realtime.createIMClient(clientID1).then(function (currClient) {
+				return currClient.createConversation({
+					members: [clientID2],
+					name: clientID1 + '&' + clientID2,
+					unique: true
+				});
+			}).then(function (conversation) {
+				self.conversation = conversation;
+				conversation.on('message', function (message) {
+					switch(message.type) {
+						case AV.TextMessage.TYPE:
+							self.addMessage(message.getText(), message.type, message.from == clientID1);
+						break;
+					}
+				});
+				self.bindEvents();
+			}).catch(console.error);
+		},
+		addMessage: function (content, type, fromMe) {
+			var self = this;
+			switch(type) {
+				case AV.TextMessage.TYPE:
+					var html = Mustache.render(self.textMsgTpl, {text: content}),
+						$html = fromMe ? $(html).addClass('me') : $(html);
+					$('.chat-list').append($html);
+					$('.chat-list').scrollTop($('.chat-list').get(0).scrollHeight);
+				break;
+			}
+		},
 		bindEvents: function () {
+			var self = this;
 			$('.chat-audio, .chat-text, .chat-material').on('click', this.onClickOption);
+			$('.text-input-section .button').on('click', function () {
+				self.onClickSendTextBtn.apply(self, arguments);
+			});
 		},
 		onClickOption: function (event) {
 			var target = event.currentTarget, 
@@ -26,7 +79,37 @@ define(function (require, exports, module) {
 				$target.removeClass('active');
 				$parent.removeClass(classList);
 			}
-		}
+		},
+		onClickSendTextBtn: function (event) {
+			var self = this, 
+				$input = $('.text-input-section input'), 
+				text = $.trim($input.val());
+
+			if(text.length == 0) {
+				$input.val('');
+				return;
+			}
+
+			self.conversation.send(new AV.TextMessage(text)).then(function () {
+				$input.val('');
+			}).catch(console.error);
+		},
+		textMsgTpl: [
+			'<div class="chat-item">',
+				'<div class="chat-avatar">',
+					'<img src="./src/img/default-avatar.png">',
+				'</div>',
+				'<div class="chat-content">',
+					'<div class="chat-user">',
+						'<span class="chat-user-name">XXX</span>',
+						'<span class="chat-user-title">XX</span>',
+					'</div>',
+					'<div class="chat-text-msg">',
+						'<p>{{text}}</p>',
+					'</div>',
+				'</div>',
+			'</div>'
+		].join('')
 	};
-	chatPage.init();
+	chatPage.initialize();
 });
