c29b3fd2aaff069e2c1215617dadb8960d46bc74
oliverbi <bixiaodong1990@163.com>
2017-01-12 03:27:18 +0800

    1. 完成主要的数据接口联调，剩余语音消息和下拉刷新聊天记录 2. 前端发布脚本实现

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,21 @@
 
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
+var gulpSync = require('gulp-sync')(gulp);
+
 var sass = require('gulp-sass');
 var sprity = require('sprity');
+
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var rev = require('gulp-rev');
+var revCollector = require('gulp-rev-collector');
+var seajsRev = require('gulp-seajs-rev');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
@@ -45,4 +58,77 @@ gulp.task('browser-sync-dev', function() {
     gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
+gulp.task('browser-sync-online', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('*.html')
+        .pipe(usemin({
+            vendorCSS: [minifyCss(), rev()],
+            styleCSS: [minifyCss(), rev()],
+            vendorJS: [uglify(), rev()]
+        }))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('copy-rev-img', function () {
+    return gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(rev())
+        .pipe(gulp.dest('dist/src/img'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('dist/src/img'));
+});
+
+gulp.task('copy-seajs-config', function () {
+    return gulp.src('src/js/seajs-config.js')
+        .pipe(gulp.dest('dist/src/js'));
+});
+
+gulp.task('rev-js-seajs', function () {
+    return gulp.src(['src/js/*.js', '!src/js/seajs-config.js'])
+        .pipe(rev())
+        .pipe(gulp.dest('dist/src/js'))
+        .pipe(rev.manifest())
+        .pipe(seajsRev({base: 'dist/src/js', configFile: 'seajs-config.js'}))
+        .pipe(gulp.dest('dist/src/js'));
+});
+
+gulp.task('rev-html', function () {
+    return gulp.src(['dist/src/js/*.json', 'dist/*.html'])
+        .pipe(revCollector({
+            replaceReved: true
+        }))
+        .pipe(minifyHtml({
+            empty: true,
+            spare: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rev-css-img', function () {
+    return gulp.src(['dist/src/img/*.json', 'dist/*.css'])
+        .pipe(revCollector({
+            replaceReved: true,
+            dirReplacements: {
+                '../img': 'src/img'
+            }
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('dev', ['browser-sync-dev', 'sass:watch']);
+
+gulp.task('build', gulpSync.sync(['clean', 'usemin', 'copy-rev-img', 'copy-seajs-config', 'rev-js-seajs', 'rev-html', 'rev-css-img']));
+
+gulp.task('online', gulpSync.sync(['build', 'browser-sync-online']));
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,73 +1,204 @@
 define(function (require, exports, module) {
-	var chatPage = {
+	var BASE_URL = 'http://stg-gambition.leanapp.cn';
+
+	// 这里需要动态获取当前微信用户的信息
+	
+		var mobilePhoneNumber = 'oHPV5v_kuprY5R3NeX6hVyhgKmkE',
+		password = '$2a$13$zol0k.bzDgefSGhQpBX/VOk.zlxuUJ3B1WNr81EW0p52CwqbUGRNa';
+	
+
+	/*
+		var mobilePhoneNumber = 'oHPV5v57NNFlk0Krwxpft5WRDk9o',
+		password = '$2a$13$x6nassMYeM6xHeXyzovBR.RMOSALOxHhwq09YYTXzpe0WF/oG9k1i';
+	*/	
+
+	var chatApp = {
+		loginUser: null,
+		isTeacher: false,
+		clazzId: '584bbb8f2f301e005724eb38',
+		studentId: null,
+		hasInitChatPage: false,
+
 		initialize: function () {
-			this.bindEvents();
-			this.initConversation();
-			$.init();
+			this.initAjax();
+			this.initPage();
+			this.initLogin();
 		},
-		initConversation: function () {
+		initAjax: function () {
+			$(document).on('ajaxSend', function () {
+				$.showIndicator();
+			});
+			$(document).on('ajaxComplete', function () {
+				$.hideIndicator();
+			});
+			$(document).on('ajaxError', function () {
+				$.toast('请求失败');
+			});
+		},
+		initLogin: function () {
 			var self = this;
 
-			// 初始化存储 SDK
-			AV.init({
-			  	appId: 'XJroNb2m8eHOGobg6WKN16K6-gzGzoHsz', 
-			  	appKey:'rGGy6ziKb0uxnfrwp6T37gmK',
+			self.ajaxLogin().done(function (data, status, xhr) {
+				if(data.code == 200) {
+					self.loginUser = data.data;
+					
+					self.ajaxCheckTeacher().done(function (data, status, xhr) {
+						if(data.code == 200) {
+							self.isTeacher = data.data.isTeacher;
+							self.initPageHtml();
+						} else {
+							$.toast('获取数据失败');
+						}
+					});
+				} else {
+					$.toast('获取数据失败');
+				}
 			});
-			// 初始化实时通讯 SDK
-			var Realtime = AV.Realtime;
-			var realtime = new Realtime({
-			  	appId: 'XJroNb2m8eHOGobg6WKN16K6-gzGzoHsz',
-			  	region: 'cn',
-			  	plugins: [AV.TypedMessagesPlugin], // 注册富媒体消息插件
-			});
+		},
+		initPage: function () {
+			var self = this, 
+				sLoading = false,
+				sPaginator = {
+					pageSize: 20,
+					pageNumber: 1
+				};
 
-			/*
-				需要替换为实际进行对话的用户的唯一标识
-			 */
-			var clientID1 = 'Oliver', clientID2 = 'Mikey';
-			realtime.createIMClient(clientID1).then(function (currClient) {
-				return currClient.createConversation({
-					members: [clientID2],
-					name: clientID1 + '&' + clientID2,
-					unique: true
-				});
-			}).then(function (conversation) {
-				self.conversation = conversation;
-				conversation.on('message', function (message) {
-					switch(message.type) {
-						case AV.TextMessage.TYPE:
-							if(message.from != clientID1)
-								self.addMessage(message.getText(), message.type, false);
-						break;
+			$(document).on('pageInit', '#student-page', function(e, pageId, $page) {
+				self.ajaxStudentList().done(function (data, status, xhr) {
+					if(data.code == 200) {
+						var dataArr = data.data.values, 
+							sPaginator = data.data.paginator, 
+							$ul = $('<ul></ul>');
+
+						$.each(dataArr, function (index, item) {
+							item.status.key = item.status.key.toLowerCase();
+						  	var html = Mustache.render(self.studentFeedbackTpl, {id: item.id, userInfo: item.userInfo, status: item.status});
+							$ul.append(html);
+						});
+						
+						$('#student-page .list-block').empty().append($ul);
+
+						if(sPaginator.pageNumber >= sPaginator.totalPageNum) {
+							$.detachInfiniteScroll($('.student-list.infinite-scroll'));
+                  			$('.student-list .infinite-scroll-preloader').remove();
+						}
+					} else {
+						$.toast('获取数据失败');
 					}
 				});
-			}).catch(console.error);
+			});
+
+			$(document).on('infinite', '.student-list', function () {
+				if(sLoading) return;
+				if(sPaginator.pageNumber >= sPaginator.totalPageNum) return;
+
+				sLoading = true;
+				sPaginator.pageNumber++;
+
+				self.ajaxStudentList(sPaginator.pageNumber, sPaginator.pageSize).done(function (data, status, xhr) {
+					if(data.code == 200) {
+						var dataArr = data.data.values, 
+							sPaginator = data.data.paginator;
+
+						$.each(dataArr, function (index, item) {
+							item.status.key = item.status.key.toLowerCase();
+						  	var html = Mustache.render(self.studentFeedbackTpl, {id: item.id, userInfo: item.userInfo, status: item.status});
+							$('#student-page .list-block ul').append(html);
+						});
+
+						if(sPaginator.pageNumber >= sPaginator.totalPageNum) {
+							$.detachInfiniteScroll($('.student-list.infinite-scroll'));
+                  			$('.student-list .infinite-scroll-preloader').remove();
+						}
+					} else {
+						$.toast('获取数据失败');
+					}
+					sLoading = false;
+					$.refreshScroller();
+				});
+			});
+
+			$(document).on('pageInit', '#chat-page', function (e, pageId, $page) {
+				if(!self.hasInitChatPage) {
+					self.ajaxMessageList().done(function (data, status, xhr) {
+						if(data.code == 200) {
+							var dataArr = data.data.replies;
+							$.each(dataArr, function (index, item) {
+								self.addMessageHtml(item);
+							});
+						} else {
+							$.toast('获取数据失败');
+						}
+					});
+					self.hasInitChatPage = true;
+				}
+			});
+
+			$(document).on('pageInit', '#material-page', function (e, pageId, $page) {
+				self.ajaxMaterialList().done(function (data, status, xhr) {
+					if(data.code == 200) {
+						var dataArr = data.data.values;
+						$.each(dataArr, function (index, item) {
+							var html = Mustache.render(self.materialContentTpl, {item: item});
+						  	$('#material-page ul').append(html);
+						});
+					} else {
+						$.toast('获取数据失败');
+					}
+				});
+			});
 		},
-		addMessage: function (content, type, fromMe) {
+		initPageHtml: function () {
 			var self = this;
-			switch(type) {
-				case AV.TextMessage.TYPE:
-					var html = Mustache.render(self.textMsgTpl, {text: content}),
-						$html = fromMe ? $(html).addClass('me') : $(html);
-					$('.chat-list').append($html);
-					$('.chat-list').scrollTop($('.chat-list').get(0).scrollHeight);
-				break;
+
+			if(self.isTeacher) {
+				$.router.load('#student-page');
+
+				$('#chat-page .bar-nav').prepend([
+					'<a class="button button-link button-nav pull-left" href="#student-page">',
+			      		'<span class="icon icon-left"></span>',
+			    	'</a>'
+				].join(''));
+
+				$('#chat-page .bar-footer').prepend([
+					'<div class="row">',
+					'	<div class="col-33 chat-audio"><i class="icon-chat icon-chat-microphone"></i>语音</div>',
+					'	<div class="col-33 chat-text"><i class="icon-chat icon-chat-keyboard"></i>文字</div>',
+					'	<div class="col-33 chat-material"><i class="icon-chat icon-chat-menu"></i>素材</div>',
+					'</div>'
+				].join(''));
+			} else {
+				$.router.load('#chat-page');
+
+				$('#chat-page .bar-footer').prepend([
+					'<div class="row">',
+					'	<div class="col-50 chat-audio"><i class="icon-chat icon-chat-microphone"></i>语音</div>',
+					'	<div class="col-50 chat-text"><i class="icon-chat icon-chat-keyboard"></i>文字</div>',
+					'</div>'
+				].join(''));
 			}
+
+			self.bindEvents();
 		},
 		bindEvents: function () {
 			var self = this;
-			$('.chat-audio, .chat-text, .chat-material').on('click', this.onClickOption);
-			$('.text-input-section .button').on('click', function () {
-				self.onClickSendTextBtn.apply(self, arguments);
-			});
+
+			$('.chat-audio, .chat-text, .chat-material').on('click', this.onClickChatOption);
+
+			$('.text-input-section .button').on('click', $.proxy(self.onClickSendTextBtn, self));
+
+			if(self.isTeacher) {
+				$('.student-list .list-block').on('click', '.item-link', $.proxy(self.onClickStudentItem, self));
+				$('.material-list .list-block').on('click', 'li', $.proxy(self.onClickMaterialItem, self));
+			}
 		},
-		onClickOption: function (event) {
+		onClickChatOption: function (event) {
 			var target = event.currentTarget, 
 				$target = $(target), $parent = $target.closest('.chat-footer'),
 				classList = 'chat-audio-active chat-text-active chat-material-active';
 
 			if($target.hasClass('chat-material')) {
-				$.router.load("#material-list");
+				$.router.load('#material-page');
 				return;
 			}
 
@@ -86,6 +217,33 @@ define(function (require, exports, module) {
 				$parent.removeClass(classList);
 			}
 		},
+		onClickStudentItem: function (event) {
+			var self = this, target = event.currentTarget, $list = $('#chat-page .chat-list');
+			
+			self.studentId = $(target).data('id');
+			self.hasInitChatPage = false;
+
+			$list.empty();
+			$.router.load('#chat-page', true);
+		},
+		onClickMaterialItem: function (event) {
+			var self = this, target = event.currentTarget;
+			
+			if($(target).hasClass('list-title')) return;
+
+			self.ajaxSendMessage({
+				replyType: 'MATERIAL',
+				materialId: $(target).data('id')
+			})
+			.done(function (data, status, xhr) {
+				if(data.code == 200) {
+					self.addMessageHtml(data.data);
+					$.router.load('#chat-page');
+				} else {
+					$.toast('发送数据失败');
+				}
+			});
+		},
 		onClickSendTextBtn: function (event) {
 			var self = this, 
 				$input = $('.text-input-section input'), 
@@ -96,27 +254,150 @@ define(function (require, exports, module) {
 				return;
 			}
 
-			self.conversation.send(new AV.TextMessage(text)).then(function (message) {
-				self.addMessage(message.getText(), message.type, true);
-				$input.val('');
-			}).catch(console.error);
+			self.ajaxSendMessage({
+				replyType: 'TEXT',
+				content: $input.val()
+			})
+			.done(function (data, status, xhr) {
+				if(data.code == 200) {
+					self.addMessageHtml(data.data);
+					$input.val('');
+				} else {
+					$.toast('发送数据失败');
+				}
+			});
 		},
+		ajaxLogin: function () {
+			return $.ajax({
+				type: 'POST',
+				url: BASE_URL + '/api/mng/auth',
+				data: {
+					mobilePhoneNumber: mobilePhoneNumber,
+					password: password
+				},
+				dataType: 'json'
+			});
+		},
+		ajaxCheckTeacher: function () {
+			return $.ajax({
+				type: 'GET',
+				url: BASE_URL + '/weh5/course/' + this.clazzId + '/isTeacher',
+				dataType: 'json'
+			});
+		},
+		ajaxStudentList: function (pageNumber, pageSize) {
+			var pNum = pageNumber || 1, pSize = pageSize || 20;
+
+			return $.ajax({
+				type: 'GET',
+				url: BASE_URL + '/weh5/course/' + this.clazzId + '/feedbacks?pageNumber=' + pNum + '&pageSize=' + pSize,
+				dataType: 'json'
+			});
+		},
+		ajaxMessageList: function () {
+			var url = this.isTeacher ? (BASE_URL + '/weh5/course/' + this.clazzId + '/feedback/' + this.studentId) : (BASE_URL + '/weh5/course/' + this.clazzId + '/feedback');
+
+			return $.ajax({
+				type: 'GET',
+				url: url,
+				dataType: 'json'
+			});
+		},
+		ajaxMaterialList: function () {
+			return $.ajax({
+				type: 'GET',
+				url: BASE_URL + '/weh5/course/' + this.clazzId + '/feedback/materials',
+				dataType: 'json'
+			});
+		},
+		ajaxSendMessage: function (data) {
+			var url = this.isTeacher ? (BASE_URL + '/weh5/course/' + this.clazzId + '/feedback/' + this.studentId) : (BASE_URL + '/weh5/course/' + this.clazzId + '/feedback');
+
+			return $.ajax({
+				type: 'POST',
+				url: url,
+				data: data,
+				dataType: 'json'
+			});
+		},
+		addMessageHtml: function (item) {
+			var self = this, $list = $('#chat-page .chat-list');
+
+			switch(item.type) {
+				case 'TEXT':
+					$list.append(Mustache.render(self.textMsgTpl, {item: item}));
+				break;
+				case 'MATERIAL':
+					$list.append(Mustache.render(self.materialMsgTpl, {item: item}));
+				break;
+			}
+			$list.scrollTop($list.get(0).scrollHeight);
+		},
+		studentFeedbackTpl: [
+			'<li>',
+				'<a class="item-link item-content" data-id="{{id}}">',
+					'<div class="item-media">',
+						'<img src="{{userInfo.headimgurl}}">',
+					'</div>',
+					'<div class="item-inner">',
+         	 			'<div class="item-title">{{userInfo.name}}</div>',
+         	 			'<div class="item-after {{status.key}}">{{status.name}}</div>',
+					'</div>',
+				'</a>',
+			'</li>'
+		].join(''),
+		materialContentTpl: [
+			'<li data-id="{{item.objectId}}">',
+	          	'<div class="item-content">',
+	            	'<div class="item-inner">',
+	              		'<div class="item-title">{{item.title}}</div>',
+	            		'<div class="item-after">{{item.author}}</div>',
+	            	'</div>',
+	          	'</div>',
+	        '</li>'
+		].join(''),
 		textMsgTpl: [
-			'<div class="chat-item">',
+			'<div class="chat-item {{#item.userInfo.isSelf}}me{{/item.userInfo.isSelf}}">',
 				'<div class="chat-avatar">',
-					'<img src="./src/img/default-avatar.png">',
+					'<img src="{{item.userInfo.headimgurl}}">',
 				'</div>',
 				'<div class="chat-content">',
 					'<div class="chat-user">',
-						'<span class="chat-user-name">XXX</span>',
-						'<span class="chat-user-title">XX</span>',
+						'{{#item.userInfo.isTeacher}}',
+						'<span class="chat-user-name">{{item.userInfo.name}}</span>',
+						'<span class="chat-user-title">笃师</span>',
+						'{{/item.userInfo.isTeacher}}',
 					'</div>',
 					'<div class="chat-text-msg">',
-						'<p>{{text}}</p>',
+						'<p>{{item.content}}</p>',
+					'</div>',
+				'</div>',
+			'</div>'
+		].join(''),
+		audioMsgTpl: [
+
+		].join(''),
+		materialMsgTpl: [
+			'<div class="chat-item {{#item.userInfo.isSelf}}me{{/item.userInfo.isSelf}}">',
+				'<div class="chat-avatar">',
+					'<img src="{{item.userInfo.headimgurl}}">',
+				'</div>',
+				'<div class="chat-content">',
+					'<div class="chat-user">',
+						'{{#item.userInfo.isTeacher}}',
+						'<span class="chat-user-name">{{item.userInfo.name}}</span>',
+						'<span class="chat-user-title">笃师</span>',
+						'{{/item.userInfo.isTeacher}}',
+					'</div>',
+					'<div class="chat-material-msg">',
+						'<div class="material-title">{{item.title}}</div>',
+						'<div class="material-line"></div>',
+						'<div class="material-author">作者: {{item.author}}<i class="icon icon-right"></i></div>',
 					'</div>',
 				'</div>',
 			'</div>'
 		].join('')
 	};
-	chatPage.initialize();
+
+	chatApp.initialize();
 });
